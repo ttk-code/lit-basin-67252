73fe02ad4aee202d635a7186c9e8780f906a27ce
tatuki.ono <tatuki.ono@techacademy.jp>
2020-03-28 19:36:11 +0000

    heroku-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,9 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-	def index
-		@tasks=Task.all
-	end
+  def index
+    @tasks = Task.all
+  end
 
   def show
   	set_task

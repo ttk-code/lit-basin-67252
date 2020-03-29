87d524bf140f486223e20e1632f77334964518be
tatuki.ono <tatuki.ono@techacademy.jp>
2020-03-29 16:01:26 +0000

    task-login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+	include SessionsHelper
+
 	private
 
   def require_user_logged_in
@@ -7,4 +9,9 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+
+  def counts(user)
+    @count_tasks = user.tasks.count
+  end
 end
+

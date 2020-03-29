123506b25758209352497c0cf87b23a5bb980bd3
tatuki.ono <tatuki.ono@techacademy.jp>
2020-03-29 09:39:23 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 	root to: 'toppages#index'
 	root to: 'tasks#index'
 
+	get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+
  resources :tasks
 end
